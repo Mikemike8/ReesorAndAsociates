da1f8023e674785fd547c035cc060415d78fb4bb
Michael Harris <158084149+Mikemike8@users.noreply.github.com>
2025-03-28 22:45:59 -0500

    push

--- Reesor-Frontend/src/main.jsx
+++ Reesor-Frontend/src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import './App.css'; // Path to your Tailwind CSS file
-
+import './App.css'; 
 import App from './App.jsx'
 
 
